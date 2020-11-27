14b8c4894de4e8ead4ecb34cc621055f4e306bdb
emiliamarkowska <emarkowska10@gmail.com>
2020-11-27 19:40:13 +0100

    backend repository init

---
+++ backend/src/main/java/com/widoczka/wodiczkapp/WodiczkappApplication.java
@@ -0,0 +1,13 @@
+package com.widoczka.wodiczkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodiczkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodiczkappApplication.class, args);
+	}
+
+}
