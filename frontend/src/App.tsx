4af1d7a175720f3b7306230515aa5d65cbf20406
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2020-11-28 00:49:31 +0100

    folders refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import {JellyFish_neutral, JellyFish_happy, JellyFish_angry} from './JellyFish/JellyFish';
 import './App.css';
+import DeviceReckogniser from './components/DeviceReckogniser';
 
 
 function App() {
-  return (
-  <div>
-    <div className="ocean">
-      <div className="wave"></div>
-      <div className="wave"></div>
-    </div>
-    <div className="jellyFish">
-    <JellyFish_happy/>
-  </div>
-    <h1 className="title">TEST</h1>
-  </div>);
+  return <DeviceReckogniser/>;
 }
 
 export default App;
---
+++ frontend/src/components/BaseDesktop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class BaseDesktop extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+      return <h1>Desktop</h1>;
+    }
+}
