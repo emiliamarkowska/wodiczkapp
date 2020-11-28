5787d9088284ecdeef6597974f8dac09e5db59e5
adamnapieralski <adam.napieralski@gmail.com>
2020-11-28 03:57:11 +0100

    add active category adding by category id

--- backend/src/main/java/com/widoczka/wodiczkapp/controllers/ActiveCategoryController.java
+++ backend/src/main/java/com/widoczka/wodiczkapp/controllers/ActiveCategoryController.java
@@ -1,7 +1,13 @@
 package com.widoczka.wodiczkapp.controllers;
 
+import com.widoczka.wodiczkapp.model.ActiveCategory;
+import com.widoczka.wodiczkapp.model.Category;
+import com.widoczka.wodiczkapp.model.CurrentUsage;
+import com.widoczka.wodiczkapp.repositories.CategoryRepository;
 import com.widoczka.wodiczkapp.services.ActiveCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +17,10 @@ public class ActiveCategoryController {
     @Autowired
     public ActiveCategoryController(ActiveCategoryService activeCategoryService) {
         this.activeCategoryService = activeCategoryService;
+    }
+
+    @PutMapping(value = "/api/active-category/{categoryId}")
+    public void addActiveCategory(@PathVariable Integer categoryId) {
+        activeCategoryService.addActiveCategoryByCategory(categoryId);
     }
 }
