75e1bd924265bcff0e48c93731020d0c7cf2ab7a
Michał Suliborski <msuliborski@gmail.com>
2020-11-28 06:22:37 +0100

    hotfix

--- backend/src/main/java/com/wodiczka/wodiczkapp/repositories/HourlyUsageRepository.java
+++ backend/src/main/java/com/wodiczka/wodiczkapp/repositories/HourlyUsageRepository.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 public interface HourlyUsageRepository extends CrudRepository<HourlyUsage, Integer> {
     @Query(value = "select round(sum(hourly_usages.sum), 2) from hourly_usages\n" +
-            "where hourly_usages.category_id = :categoryId and hour(hourly_usages.date_time) = (hour(dateTime));", nativeQuery = true)
+            "where hourly_usages.category_id = :categoryId and hour(hourly_usages.date_time) = (hour(:dateTime));", nativeQuery = true)
     float getCurrentUsageInLiters(@Param("categoryId") Integer categoryId,
                                   @Param("dateTime") LocalDateTime dateTime);
 }
