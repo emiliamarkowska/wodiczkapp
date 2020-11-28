cffe5301a79285df3c4ac93aca547be91ae99d68
adamnapieralski <adam.napieralski@gmail.com>
2020-11-28 22:30:48 +0100

    add backend api url to env variable

--- frontend/src/Services/UsageService.jsx
+++ frontend/src/Services/UsageService.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export async function getDailyUsage() {
-    return await axios.get('http://10.3.145.165:8080/api/usage/current-day').then(data => data.data);
+    return await axios.get(`${process.env.REACT_APP_API_BASE_URL}/usage/current-day`).then(data => data.data);
 }
 
 export async function getHistoryUsage() {
-    return await axios.get('http://10.3.145.165:8080/api/usage/days/days_amount/7').then(data => data);
+    return await axios.get(`${process.env.REACT_APP_API_BASE_URL}/usage/days/days_amount/7`).then(data => data);
 }
