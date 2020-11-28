dd7ea07f7e68c8a23750d6733b173d5e67bc4d06
emiliamarkowska <emarkowska10@gmail.com>
2020-11-28 02:44:12 +0100

    commit

--- backend/src/main/java/com/widoczka/wodiczkapp/repositories/CategoryRepository.java
+++ backend/src/main/java/com/widoczka/wodiczkapp/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.widoczka.wodiczkapp.repositories;
 
 import com.widoczka.wodiczkapp.model.CategoryLabel;
-import com.widoczka.wodiczkapp.model.Record;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<CategoryLabel, Integer> {
